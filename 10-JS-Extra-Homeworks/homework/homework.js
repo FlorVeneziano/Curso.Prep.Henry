7fe0a1274515f846df7c7e0baf3df6053a0afcfe
FlorVeneziano <flopy2081999@gmail.com>
2021-11-04 22:48:11 -0300

    homework part1

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  const matriz = []
+      for(i in objeto) {
+    matriz.push([i, objeto[i]]);
+  }
+  return matriz
 }
 
 
